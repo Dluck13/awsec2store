7dfd6a0aab99de0227626d37f0973e7e2661c17a
DNL0815 <dimitri.luck@outlook.com>
2023-06-12 20:51:28 -0400

    Feature one complete, exposeIds

--- src/main/java/com/ecommerce/store/repository/ProductRepository.java
+++ src/main/java/com/ecommerce/store/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.ecommerce.store.repository;
 
 import com.ecommerce.store.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
 
 public interface ProductRepository extends JpaRepository<Product,Long> {
 
