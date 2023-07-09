cdfaeec091d063660a2c0042e7cd101b2bf92fd9
DNL0815 <dimitri.luck@outlook.com>
2023-07-09 19:58:43 -0400

    cors

--- src/main/java/com/ecommerce/store/repository/ProductRepository.java
+++ src/main/java/com/ecommerce/store/repository/ProductRepository.java
@@ -3,8 +3,9 @@ package com.ecommerce.store.repository;
 import com.ecommerce.store.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-
+@CrossOrigin("*")
 public interface ProductRepository extends JpaRepository<Product,Long> {
 
 
