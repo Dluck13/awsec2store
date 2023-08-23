bd7d4e5fe35edc1648df0fe076e9b242ee41e839
DNL0815 <dimitri.luck@outlook.com>
2023-08-23 19:15:32 -0400

    Feature three complete, search by category

--- src/main/java/com/ecommerce/store/repository/ProductRepository.java
+++ src/main/java/com/ecommerce/store/repository/ProductRepository.java
@@ -1,12 +1,18 @@
 package com.ecommerce.store.repository;
 
 import com.ecommerce.store.entity.Product;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import org.springframework.data.domain.Pageable;
 
 @CrossOrigin("*")
 public interface ProductRepository extends JpaRepository<Product,Long> {
+    @RestResource(path = "categoryid")
+    Page<Product> findByCategoryId(@Param("id")Long id, Pageable pageable);
 
 
 }
